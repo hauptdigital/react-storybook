ea948f2a3ed5a28997cc0b8caad1bd25d0af0568
hauptdigital <marc@haupt.digital>
2020-03-31 22:05:48 +0200

    Finish UserPassword component functionality

---
+++ src/components/UserPassword.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const PasswordInput = styled.input`
+  background-color: hsla(
+    ${props => props.hslaValues.hue},
+    100%,
+    67%,
+    ${props => props.hslaValues.lightness}
+  );
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 1px 0px black;
+  padding: 15px;
+  font-size: 2em;
+`;
+
+function UserPassword({ defaultValue }) {
+  const [hslaValues, setHslaValues] = React.useState({ hue: 0, lightness: 0 });
+
+  function setInputBackgroundColor(inputValue) {
+    const numberOfCharacters = inputValue.length;
+    const updatedHslaValues = {
+      hue: numberOfCharacters >= 12 ? 120 : numberOfCharacters * 10,
+      lightness: numberOfCharacters === 0 ? 0 : 1
+    };
+    setHslaValues(updatedHslaValues);
+  }
+
+  React.useEffect(() => {
+    if (defaultValue) {
+      setInputBackgroundColor(defaultValue);
+    }
+  }, []);
+
+  return (
+    <PasswordInput
+      type="password"
+      onChange={event => {
+        setInputBackgroundColor(event.target.value);
+      }}
+      hslaValues={hslaValues}
+      defaultValue={defaultValue}
+    />
+  );
+}
+
+export default UserPassword;
--- src/stories/2-Input.stories.js
+++ src/stories/2-Input.stories.js
@@ -3,4 +3,7 @@ import UserPassword from "../components/userPassword";
 
 export default { title: "UserPassword" };
 
-export const typePassword = () => <UserPassword />;
+export const empty = () => <UserPassword defaultValue="" />;
+export const notSafe = () => <UserPassword defaultValue="4444" />;
+export const Safe = () => <UserPassword defaultValue="1010101010" />;
+export const Safer = () => <UserPassword defaultValue="121212121212" />;
