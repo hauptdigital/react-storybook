870241dd6149829ab42388dfd3ec46103a5c9c7b
hauptdigital <marc@haupt.digital>
2020-04-01 21:19:51 +0200

    Refactor PasswordInput component

--- src/components/UserPassword.js
+++ src/components/UserPassword.js
@@ -1,44 +1,29 @@
-import React from "react";
 import styled from "@emotion/styled";
 
 const PasswordInput = styled.input`
-  background-color: hsla(
-    ${props => props.hslaValues.hue},
-    100%,
-    67%,
-    ${props => props.hslaValues.alpha}
-  );
   border: none;
   outline: none;
   box-shadow: 0px 0px 1px 0px black;
   padding: 15px;
   font-size: 2em;
+  &:focus {
+    background-color: ${props =>
+      `hsla(${calculateHue(props.value.length)}, 100%, 50%, ${calculateAlpha(
+        props.value.length
+      )})`};
+  }
 `;
 
-function UserPassword({ value }) {
-  const [hslaValues, setHslaValues] = React.useState({ hue: 0, alpha: 0 });
-  const [inputValue, setInputValue] = React.useState(value ? value : "");
-
-  function setInputBackgroundColor(inputValue) {
-    const numberOfCharacters = inputValue.length;
-    const updatedHslaValues = {
-      hue: numberOfCharacters >= 12 ? 120 : numberOfCharacters * 10,
-      alpha: numberOfCharacters === 0 ? 0 : 1
-    };
-    setInputValue(inputValue);
-    setHslaValues(updatedHslaValues);
-  }
-
-  return (
-    <PasswordInput
-      type="password"
-      onChange={event => {
-        setInputBackgroundColor(event.target.value);
-      }}
-      hslaValues={hslaValues}
-      value={inputValue}
-    />
-  );
+function calculateHue(passwordLength) {
+  return Math.min(passwordLength * 10, 120);
 }
 
-export default UserPassword;
+function calculateAlpha(passwordLength) {
+  return passwordLength === 0 ? 0 : 1;
+}
+
+PasswordInput.defaultProps = {
+  type: "password"
+};
+
+export default PasswordInput;
--- src/stories/2-Input.stories.js
+++ src/stories/2-Input.stories.js
@@ -3,7 +3,42 @@ import UserPassword from "../components/UserPassword";
 
 export default { title: "UserPassword" };
 
-export const empty = () => <UserPassword value="" />;
-export const notSafe = () => <UserPassword value="4444" />;
-export const Safe = () => <UserPassword value="1010101010" />;
-export const Safer = () => <UserPassword value="121212121212" />;
+export const Empty = () => {
+  const [value, setValue] = React.useState("");
+  return (
+    <UserPassword
+      onChange={event => setValue(event.target.value)}
+      value={value}
+    />
+  );
+};
+
+export const NotSafe = () => {
+  const [value, setValue] = React.useState("4444");
+  return (
+    <UserPassword
+      onChange={event => setValue(event.target.value)}
+      value={value}
+    />
+  );
+};
+
+export const Safe = () => {
+  const [value, setValue] = React.useState("1010101010");
+  return (
+    <UserPassword
+      onChange={event => setValue(event.target.value)}
+      value={value}
+    />
+  );
+};
+
+export const Safer = () => {
+  const [value, setValue] = React.useState("121212121212");
+  return (
+    <UserPassword
+      onChange={event => setValue(event.target.value)}
+      value={value}
+    />
+  );
+};
